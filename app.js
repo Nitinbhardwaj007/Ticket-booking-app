c5ba840326e54045941f5264505777efcde40d1f
Nitinbhardwaj007 <imnitinbhardwaj786@gmail.com>
2025-12-24 23:58:46 +0530

    updated app js

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const errorHandler = require('./middlewares/errorMiddleware')
+const authrouter = require('./routers/authrouter')
+const userrouter = require('./routers/userrouter')
 
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(errorHandler)
+app.use('/api/auth', authrouter)
+app.use('/api/users', userrouter)
 
 module.exports = { app }
