03f0160d518f450904335d76547bb95adb3c5a83
Nitinbhardwaj007 <imnitinbhardwaj786@gmail.com>
2026-01-07 19:24:35 +0530

    updated app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const errorHandler = require('./middlewares/errorMiddleware')
 const authrouter = require('./routers/authrouter')
 const userrouter = require('./routers/userrouter')
+const ticketrouter = require('./routers/ticketrouter')
 
 const app = express()
 app.use(express.json())
@@ -9,5 +10,6 @@ app.use(express.urlencoded({ extended: true }))
 app.use(errorHandler)
 app.use('/api/auth', authrouter)
 app.use('/api/users', userrouter)
+app.use('/api/ticket', ticketrouter)
 
 module.exports = { app }
