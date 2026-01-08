55312df426cb192bd7303b5849b741b987c996ea
Nitinbhardwaj007 <imnitinbhardwaj786@gmail.com>
2026-01-08 21:30:07 +0530

    update ticket router

--- routers/ticketrouter.js
+++ routers/ticketrouter.js
@@ -1,9 +1,19 @@
 const express = require('express')
-const { createTicket } = require('../controllers/ticket.controller')
+const {
+  createTicket,
+  getTickets,
+  updateTicket,
+  deleteTicket,
+  getTicketById,
+} = require('../controllers/ticket.controller')
 const authMiddleware = require('../middlewares/auth.middleware')
 
 const router = express.Router()
 
 router.post('/', authMiddleware, createTicket)
+router.get('/', authMiddleware, getTickets)
+router.put('/:id', authMiddleware, updateTicket)
+router.delete('/:id', authMiddleware, deleteTicket)
+router.get('/:id', authMiddleware, getTicketById)
 
 module.exports = router
